aeadaca352d5fd04622fb0b042310077aa8062c5
Andrey <truan35@yandex.ru>
2022-11-17 18:44:30 +0300

    Added listview

--- DigitalSecretary/DigitalSecretary/src/main/java/com/baza/digitalsecretary/AppController.java
+++ DigitalSecretary/DigitalSecretary/src/main/java/com/baza/digitalsecretary/AppController.java
@@ -1,16 +1,21 @@
 package com.baza.digitalsecretary;
 
+import javafx.collections.ObservableArray;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.collections.ObservableList;
+import javafx.collections.FXCollections;
 
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 import static com.baza.digitalsecretary.HelloApplication.primaryStage;
 
@@ -26,6 +31,12 @@ public class AppController {
     private Button GoToAddEventButton;
 
     @FXML
+    private ListView<String> TodayEventListBox;
+
+    @FXML
+    private ListView<String> СomingEventsListBox;
+
+    @FXML
     void initialize() {
 
         //Обновление даты в приложении
@@ -33,7 +44,12 @@ public class AppController {
         SimpleDateFormat formatToday = new SimpleDateFormat("'Сегодня: 'dd.MM.yyyy");
         TodayText.setText(formatToday.format(today));
 
-        //ArrayList<String> allEventsList = new
+        ObservableList<String> allEventsList = FXCollections.observableArrayList();
+        allEventsList.add("17.11.2022 категория пррррррррррррррррррррррррррррррррррррррррррр");
+        allEventsList.add("24.02.2022 СВО Начало");
+        allEventsList.add("18.12.2020 Дора Релиз альбома");
+
+        TodayEventListBox.setItems(allEventsList);
 
         //Переход на авторизацию
         ExitButton.setOnAction(event -> {
