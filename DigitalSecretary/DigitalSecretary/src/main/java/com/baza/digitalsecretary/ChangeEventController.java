d59c2b25f05e615b5fb7bbd9cb6ac5633e6bbc95
Andrey <truan35@yandex.ru>
2022-11-28 19:27:19 +0300

    Update change event frame

--- DigitalSecretary/DigitalSecretary/src/main/java/com/baza/digitalsecretary/ChangeEventController.java
+++ DigitalSecretary/DigitalSecretary/src/main/java/com/baza/digitalsecretary/ChangeEventController.java
@@ -1,4 +1,82 @@
 package com.baza.digitalsecretary;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+
+import java.io.IOException;
+import java.time.LocalDate;
+
+import static com.baza.digitalsecretary.HelloApplication.primaryStage;
+
 public class ChangeEventController {
+
+    @FXML
+    private Button BackToChooseEventButton;
+
+    @FXML
+    private TextField CategoryField;
+
+    @FXML
+    private Button ChangeEventButton;
+
+    @FXML
+    private DatePicker DateField;
+
+    @FXML
+    private TextArea DescriptionField;
+
+    @FXML
+    private Label ErrorText;
+
+    @FXML
+    private TextField TitleField;
+
+    @FXML
+    void initialize() {
+
+        //нужно получить поля события, id которого был выбран в предыдущем окне
+        //и установить значения как дальше
+        DateField.setValue(LocalDate.of(2000, 1, 1));
+        TitleField.setText("название");
+        CategoryField.setText("категория");
+        DescriptionField.setText("описание");
+
+
+        ChangeEventButton.setOnAction(event -> {
+            //взять все поля и перезаписать событие
+            String message = "success";
+            if (message == "success") {
+                ErrorText.setTextFill(Color.color(0, 0.70, 0));
+                ErrorText.setText("Изменение прошло успешно");
+            } else {
+                ErrorText.setTextFill(Color.color(1, 0, 0));
+                ErrorText.setText(message);
+            }
+        });
+
+        BackToChooseEventButton.setOnAction(event -> {
+
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("choose_event.fxml"));
+
+            try {
+                loader.load();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            Parent root = loader.getRoot();
+            primaryStage.setScene(new Scene(root));
+        });
+
+    }
+
 }
--- DigitalSecretary/DigitalSecretary/src/main/java/com/baza/digitalsecretary/ChooseEventController.java
+++ DigitalSecretary/DigitalSecretary/src/main/java/com/baza/digitalsecretary/ChooseEventController.java
@@ -1,13 +1,6 @@
 package com.baza.digitalsecretary;
 
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -16,13 +9,10 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
-import javafx.collections.ObservableList;
-import javafx.collections.FXCollections;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
 
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
 
 import static com.baza.digitalsecretary.HelloApplication.primaryStage;
 
@@ -41,12 +31,46 @@ public class ChooseEventController {
     private TextField IdField;
 
     @FXML
+    private ListView<String> EventsListBox;
+
+    @FXML
     void initialize() {
 
+        //нужно получить List строк событий для id пользователя который вошел
+        //и добавить их в allEventsList следующим образом с id события
+        ObservableList<String> allEventsList = FXCollections.observableArrayList();
+        allEventsList.add("1 01.01.2000 название категория описание");
+        allEventsList.add("2 01.01.2000 название категория описание");
+        allEventsList.add("3 01.01.2000 название категория описание");
+        EventsListBox.setItems(allEventsList);
+
         GoToChangeButton.setOnAction(event -> {
+            //В message записывается результат проверки IdField.getText()
+            //на существование такого id события и корректности int
+            String message = "success";
+            if (message == "success") {
+                FXMLLoader loader = new FXMLLoader();
+                loader.setLocation(getClass().getResource("change_event.fxml"));
+
+                try {
+                    loader.load();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
+                Parent root = loader.getRoot();
+                primaryStage.setScene(new Scene(root));
+            } else {
+                ErrorText.setTextFill(Color.color(1, 0, 0));
+                ErrorText.setText(message);
+            }
+
+        });
+
+        BackToAppButton.setOnAction(event -> {
 
             FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(getClass().getResource("change_event.fxml"));
+            loader.setLocation(getClass().getResource("app.fxml"));
 
             try {
                 loader.load();
