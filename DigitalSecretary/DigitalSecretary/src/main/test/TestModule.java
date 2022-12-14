7c825b4b9f12a8e031523e10cf6a43795f4ea7de
Andrey <truan35@yandex.ru>
2022-12-14 15:11:01 +0300

    Added simple authorization tests

--- DigitalSecretary/DigitalSecretary/src/main/test/TestModule.java
+++ DigitalSecretary/DigitalSecretary/src/main/test/TestModule.java
@@ -5,8 +5,25 @@ import org.junit.jupiter.api.Test;
 public class TestModule {
 
     @Test
-    public void justTest() {
-        Assertions.assertEquals("success", DataManager.CheckAuthorization("admin", "root"));
+    public void successLogInTest() {
+        String login = "admin";
+        String password = "root";
+
+        String expected = "success";
+        String actual = DataManager.CheckAuthorization(login, password);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void notSuccessLogInTest() {
+        String login = "admin1234";
+        String password = "root";
+
+        String expected = "Логин еще не зарегистрирован";
+        String actual = DataManager.CheckAuthorization(login, password);
+
+        Assertions.assertEquals(expected, actual);
     }
 
 }
